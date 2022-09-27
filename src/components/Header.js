36f3c1825a8c5cbed3ebf08dc845a051d55f48d5
Sander Tonheim <sandert.kodehode@gmail.com>
2022-09-27 14:26:51 +0200

    image added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { StyledHeaderContainer } from '../styles/Header.styled'
+import { ImageContainer } from '../styles/HeaderImg.styled'
+import { HeaderImage } from './HeaderImg'
 
 export const Header = () => {
 	return (
 		<StyledHeaderContainer>
 			<h1>Portfolio</h1>
+			<HeaderImage></HeaderImage>
 		</StyledHeaderContainer>
 	)
 }
