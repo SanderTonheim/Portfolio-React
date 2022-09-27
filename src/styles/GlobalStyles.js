d303cd984f1b6cef4ec620a80949cefd834a719c
Sander Tonheim <sandert.kodehode@gmail.com>
2022-09-27 07:17:53 -0400

    GlobalStyles added, folder structur added

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+ * {
+  margin: 0%;
+	padding: 0%;
+ }
+
+  html  {
+	width: 100%;
+	max-height: 100%;
+	font-family:  sans-serif;
+}
+
+body {
+	margin: 0px;
+	background-color: #4e4e4e;
+}
+`
