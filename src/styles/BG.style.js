6fd0977c51eb58f25edf192c4224bf287d28e41a
Sander Tonheim <sandert.kodehode@gmail.com>
2022-09-29 09:51:48 +0200

    Spacer position edited to keep content flow

--- src/styles/BG.style.js
+++ src/styles/BG.style.js
@@ -3,8 +3,6 @@ import styled from 'styled-components'
 export const BGStyled = styled.div`
 	width: 100%;
 	padding: 0;
-	margin: 0;
-	position: absolute;
 	max-height: fit-content;
 
 	#flip {
@@ -13,7 +11,5 @@ export const BGStyled = styled.div`
 		/* translate: (0, -100); */
 		position: relative;
 		top: -5px;
-		z-index: -1;
-		left: 0;
 	}
 `
