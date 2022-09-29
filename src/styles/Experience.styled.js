b4f6ba8e10fc91aa1e25796414ca8df0fc8afc55
Sander Tonheim <sandert.kodehode@gmail.com>
2022-09-29 10:05:11 +0200

    Experience style added

---
+++ src/styles/Experience.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const ExperienceContainer = styled.div`
+	max-width: 100%;
+	margin: 2rem;
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+`
