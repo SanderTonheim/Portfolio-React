b65761f949694cedbc6427abc97ce9c2c1cc0e84
Sander Tonheim <sandert.kodehode@gmail.com>
2022-09-29 16:46:20 +0200

    animation

--- src/components/WaveSplitter.js
+++ src/components/WaveSplitter.js
@@ -2,11 +2,11 @@ import { WaveAnimation } from '../styles/WaveSplitter.styled'
 
 export const WaveSplitter = () => {
 	return (
-		<WaveAnimation>
+		<>
 			<div class='ocean'>
 				<div class='wave'></div>
 				<div class='wave'></div>
 			</div>
-		</WaveAnimation>
+		</>
 	)
 }
--- src/styles/WaveSplitter.styled.js
+++ src/styles/WaveSplitter.styled.js
@@ -1,46 +1,5 @@
 import styled from 'styled-components'
 
 export const WaveAnimation = styled.div`
-	.ocean {
-		height: 5rem;
-		width: 100%;
-		position: relative;
-		background: #015871;
-	}
-
-	.wave {
-		background: url(../assets/experience-background.svg) repeat-x;
-		position: absolute;
-		top: -198px;
-		width: 6400px;
-		height: 198px;
-		animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite;
-		transform: translate3d(0, 0, 0);
-	}
-
-	.wave:nth-of-type(2) {
-		top: -175px;
-		animation: wave 7s cubic-bezier(0.36, 0.45, 0.63, 0.53) -0.125s infinite,
-			swell 7s ease -1.25s infinite;
-		opacity: 1;
-	}
-
-	@keyframes wave {
-		0% {
-			margin-left: 0;
-		}
-		100% {
-			margin-left: -1600px;
-		}
-	}
-
-	@keyframes swell {
-		0%,
-		100% {
-			transform: translate3d(0, -25px, 0);
-		}
-		50% {
-			transform: translate3d(0, 5px, 0);
-		}
-	}
+	
 `
