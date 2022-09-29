93e0dc297555484ecf6780458b043c7f8eebc01a
Sander Tonheim <sandert.kodehode@gmail.com>
2022-09-29 09:45:33 +0200

    seem between svgs removed

--- src/styles/BG.style.js
+++ src/styles/BG.style.js
@@ -1,19 +1,19 @@
 import styled from 'styled-components'
 
 export const BGStyled = styled.div`
-	height: 300px;
 	width: 100%;
 	padding: 0;
 	margin: 0;
 	position: absolute;
+	max-height: fit-content;
 
 	#flip {
 		transform: scaleX(-1);
 		transform: rotate(0.5turn);
 		/* translate: (0, -100); */
 		position: relative;
-		top: -0.9rem;
+		top: -5px;
+		z-index: -1;
 		left: 0;
-		object-position: -20px;
 	}
 `
