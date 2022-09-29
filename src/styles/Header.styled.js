2442c1ddec3d70249e142318171ec6b0ec842fda
Sander Tonheim <sandert.kodehode@gmail.com>
2022-09-29 09:40:54 +0200

    none

--- src/styles/Header.styled.js
+++ src/styles/Header.styled.js
@@ -2,16 +2,16 @@ import styled from 'styled-components'
 
 export const StyledHeaderContainer = styled.div`
 	max-width: 100%;
-	height: 35rem;
+	height: fit-content;
 	display: flex;
 	flex-direction: column;
 	margin: 2rem 2rem;
+	margin-bottom: 0%;
 	align-items: center;
 	gap: 2rem;
-	overflow: hidden;
-	background-image: url('../assets/layered-waves-haikei.svg');
 
 	h1 {
 		width: 100%;
 	}
+
 `
